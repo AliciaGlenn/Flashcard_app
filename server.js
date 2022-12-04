f8f4ba20395df041cafa436830ecdb82c2352848
Alicia Glenn <alicia@aliciaglenn.com>
2022-12-04 08:19:20 -0500

    Installed dependencies dotenv, ejs, express, method-override, mongoose, Morgan Created route directories Imported dependencies to server.js Created express app in server.js Established mongo connection  & connection events in server.js Registered middleware in server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+const MONGO = process.env.Mongo;
+
+// Create express app
+const app = express();
+
+// establish mongo connection
+mongoose.connect(process.env.MONGO);
+
+// mongoose connection events
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+// register middleware
+app.use(morgan("dev"));
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+  res.send("your server is running... better catch it.");
+});
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
