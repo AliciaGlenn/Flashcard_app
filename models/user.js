1d3c0742da3707fd6ae40f71f1550ebd0dcc3028
AliciaGlenn <Alicia@AliciaGlenn.com>
2022-12-15 14:43:45 -0500

    fixed sign up because it wasn't pushing to mongo, removed some schema properties, also updated the login page

--- models/user.js
+++ models/user.js
@@ -11,9 +11,9 @@ const { Schema, model } = mongoose;
 
 // make flashcard schema
 const userSchema = new Schema({
-  first_name: { type: String, required: false, unique: true },
-  last_name: { type: String, required: false, unique: true },
-  email: { type: String, required: true, unique: true },
+  // first_name: { type: String, required: false, unique: true },
+  // last_name: { type: String, required: false, unique: true },
+  // email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
